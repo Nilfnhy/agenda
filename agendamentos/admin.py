5c0941d569b36cfee8603a98df77a168b144b24f
Nicole Mello <editionsns@gmail.com>
2024-11-13 16:08:20 -0300

    home

--- agendamentos/admin.py
+++ agendamentos/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from agendamentos.models import Agendamento
+
+
 # Register your models here.
+
+@admin.register(Agendamento)
+class AgendamentoAdmin(admin.ModelAdmin):
+    list_display = ('horario', 'cliente', 'funcionario', 'valor')
+    search_fields = ('ciente', 'funcionario')
+    list_filter = ('ciente', 'servico')
--- fornecedores/admin.py
+++ fornecedores/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from fornecedores.models import Fornecedor
+
+
 # Register your models here.
+
+@admin.register(Fornecedor)
+class FornecedorAdmin(admin.ModelAdmin):
+    list_display = ('nome', 'cnpj', 'fone')
+    search_fields = ('nome', 'fone')
